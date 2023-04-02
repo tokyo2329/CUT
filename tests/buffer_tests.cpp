9adcb9ac02c5334acd0a4f0a9d60ddf5c8dfc52f
tokyo2329 <mateusz.kociola@icloud.com>
2023-04-02 21:47:41 +0200

    added unit tests for buffer

---
+++ tests/buffer_tests.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <assert.h>
+#include <pthread.h>
+
+extern "C" {
+  #include "buffer.h"
+}
+
+
+TEST(BufferTest, Init) {
+  Buffer b;
+  init_buffer(&b);
+
+  // try to lock and unlock the initialized mutex
+  pthread_mutex_lock(&b.mtx);
+  pthread_mutex_unlock(&b.mtx);
+
+  destroy_buffer(&b);
+}
+
+TEST(BufferTest, Write) {
+  Buffer b;
+  init_buffer(&b);
+
+  char * test_string = "Hello world!";
+  write_to_buffer(&b, test_string);
+  
+  char * result = read_from_buffer(&b);
+  
+  EXPECT_STREQ(result, test_string);
+  
+  destroy_buffer(&b);
+}
+
+TEST(BufferTest, Read) {
+  Buffer b;
+  init_buffer(&b);
+
+  char * test_string = "Hello world!";
+  write_to_buffer(&b, test_string);
+
+  char * result = read_from_buffer(&b);
+  
+  EXPECT_STREQ(result, test_string);
+
+  destroy_buffer(&b);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
