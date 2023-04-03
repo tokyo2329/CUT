35bc58974825e0c99dba2358b925fb4d1035d81f
tokyo2329 <mateusz.kociola@icloud.com>
2023-04-03 20:00:43 +0200

    use strdup in read_from_buffer instead

--- src/buffer.c
+++ src/buffer.c
@@ -22,15 +22,9 @@ void write_to_buffer(Buffer * b, char * d) {
 char * read_from_buffer(Buffer * b) {
   pthread_mutex_lock(&b->mtx);
 
-  char * value = malloc(BUFFER_SIZE);
-  if(value == NULL) {
-    printf("Failed to allocate memory!\n");
-    exit(1);
-  }
-
-  strcpy(value, b->data);
+  char * from_buffer = strdup(b->data);
 
   pthread_mutex_unlock(&b->mtx);
 
-  return value;
+  return from_buffer;
 }
