339e38738b6838f8672002adfbd0a3ff00d63c19
tokyo2329 <mateusz.kociola@icloud.com>
2023-04-05 15:22:23 +0200

    add usage of semaphores to buffer

--- src/buffer.c
+++ src/buffer.c
@@ -1,29 +1,32 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <string.h>
 #include <stdlib.h>
 #include "buffer.h"
 
-void init_buffer(Buffer * b) {
+void init_buffer(buffer * b) {
   pthread_mutex_init(&b->mtx, NULL);
+  sem_init(&b->sp, 0, 0);
 }
 
-void destroy_buffer(Buffer * b) {
+void destroy_buffer(buffer * b) {
   pthread_mutex_destroy(&b->mtx);
+  sem_destroy(&b->sp);
 }
 
-void write_to_buffer(Buffer * b, char * d) {
+void write_to_buffer(buffer * b, char * d) {
   pthread_mutex_lock(&b->mtx);
-
   strcpy(b->data, d);
-
   pthread_mutex_unlock(&b->mtx);
+  
+  sem_post(&b->sp);
 }
 
-char * read_from_buffer(Buffer * b) {
+char * read_from_buffer(buffer * b) {
+  sem_wait(&b->sp);
+
   pthread_mutex_lock(&b->mtx);
-
   char * from_buffer = strdup(b->data);
-
   pthread_mutex_unlock(&b->mtx);
 
   return from_buffer;
